266847e9aa3b5661e90e12899256b3ba28ab36c2
DriftingShade <sarus333@yahoo.com>
2024-09-11 22:54:34 -0600

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "react-weather-app",
+  base: "World-Weather-Network",
   server: {
     port: 3000,
   },
